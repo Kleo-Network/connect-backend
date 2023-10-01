0639f6016108be7923b72029360dce9fd3e5ca30
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2023-10-01 18:26:19 +0530

    APIS

--- app/core/database.py
+++ app/core/database.py
@@ -1,6 +1,8 @@
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
-
+import json
+from decimal import Decimal
+import random 
 aws_access_key = "os.environ.get('AWS_ACCESS_KEY_ID')"
 aws_secret_access_key = "os.environ.get('AWS_SECRET_ACCESS_KEY')"
 aws_region = "ap-south-1"
@@ -28,9 +30,34 @@ def upload_browsing_data(item,user_id):
     table = dynamodb.Table('history')
     item["user_id"] = user_id
     item["favourite"] = False
-    table.put_item(Item=item)
+    item["id"] = str(random.randint(1, 10000000))
+    item = json.loads(json.dumps(item), parse_float=Decimal)
+
+    print("start")
+    res = table.put_item(Item=item)
+    print(item)
+    print("uploaded")
     return True
 
+def get_history(user_id, from_epoch, to_epoch, regex):
+    table = dynamodb.Table('history')
+    response = table.query(
+        KeyConditionExpression=(
+            "user_id = :user_id AND #time_attr BETWEEN :start_date AND :end_date"
+        ),
+        FilterExpression="contains (#name_attr, :username)",
+        ExpressionAttributeNames={
+            "#time_attr": "visitTime",
+            "#name_attr": "url"
+        },
+        ExpressionAttributeValues={
+            ":user_id": user_id,
+            ":username": regex,
+            ":start_date": from_epoch,
+            ":end_date": to_epoch
+        }
+    )
+    return response['Items']
 
 def query_history(user_id, filters):
     table = dynamodb.Table('history')
@@ -55,4 +82,37 @@ def delete_history_item(primary_id):
     response = table.delete_item(Key={"item_id": primary_id })
     return response 
 
+def get_pinned_website(user_id):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('pinned_websites')
+    
+    # Query the table for the given user_id
+    response = table.query(
+        KeyConditionExpression="user_id = :user_id",
+        ExpressionAttributeValues={
+            ":user_id": user_id
+        }
+    )
+    
+    # Return the items from the response
+    return response['Items']
 
+def add_to_pinned_websites(user_id, domain_name, order):
+    """
+    Add a new item to the 'pinned_websites' DynamoDB table.
+
+    :param user_id: The user ID.
+    :param domain_name: The domain name.
+    :param order: The order as an integer.
+    """
+    table = dynamodb.Table('pinned_websites')
+    
+    item = {
+        'user_id': user_id,
+        'domain_name': domain_name,
+        'order': order
+    }
+    
+    response = table.put_item(Item=item)
+    
+    return response
