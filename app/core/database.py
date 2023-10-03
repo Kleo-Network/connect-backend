72d7191ee71a5b2375ba62534eed7939eb4b93b0
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2023-10-04 00:02:08 +0530

    graph query sidebar

--- app/core/database.py
+++ app/core/database.py
@@ -5,7 +5,8 @@ from decimal import Decimal
 import random 
 from collections import defaultdict
 import uuid
-
+import tldextract
+import datetime
 
 aws_access_key = "AKIA3RWDXTFSIADMEAPE"
 aws_secret_access_key = "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"
@@ -35,6 +36,11 @@ def upload_browsing_data(item,user_id):
     item["user_id"] = user_id
     item["favourite"] = False
     item["id"] = str(random.randint(1, 10000000))
+    
+    extracted = tldextract.extract(item["url"])
+    domain = "{}.{}".format(extracted.domain, extracted.suffix)
+    item["icon"] = "https://www.google.com/s2/favicons?domain={}&sz=48".format(domain)
+    
     item = json.loads(json.dumps(item), parse_float=Decimal)
 
     print("start")
@@ -127,11 +133,36 @@ def get_summary(user_id, domain_name):
     )
     return response['Items']
 
-def get_grpah_query(user_id, fromEpoch, toEpoch, divisions):
+def get_time_batch(unix_timestamp):
+    # Convert the timestamp to datetime object
+    dt = datetime.datetime.fromtimestamp(unix_timestamp / 1000.0)  # divide by 1000 to convert ms to s
+    hour = dt.hour
+
+    # Determine the batch
+    if 0 <= hour < 4:
+        return "00-04"
+    elif 4 <= hour < 8:
+        return "04-08"
+    elif 8 <= hour < 12:
+        return "08-12"
+    elif 12 <= hour < 16:
+        return "12-16"
+    elif 16 <= hour < 20:
+        return "16-20"
+    else:
+        return "20-24"
+def get_graph_query(user_id, fromEpoch, toEpoch):
+    # Initial empty structure
+    result = {
+        "00-04": [],
+        "04-08": [],
+        "08-12": [],
+        "12-16": [],
+        "16-20": [],
+        "20-24": []
+        
+    }
     table = dynamodb.Table('history')
-    fromEpoch = Decimal(fromEpoch)
-    toEpoch = Decimal(toEpoch)
-    divisions = Decimal(divisions)
     response = table.scan(
     FilterExpression="user_id = :user_id AND #visitTime BETWEEN :fromEpoch AND :toEpoch",
     ExpressionAttributeNames={
@@ -139,22 +170,113 @@ def get_grpah_query(user_id, fromEpoch, toEpoch, divisions):
     },
     ExpressionAttributeValues={
         ":user_id": user_id,
-        ":fromEpoch": fromEpoch,
-        ":toEpoch": toEpoch
-    }
-)
+        ":fromEpoch": Decimal(fromEpoch),
+        ":toEpoch": Decimal(toEpoch),
+    })
+    data = response['Items']
+    print(data)
+    # Iterate over each item in the database
+    for item in data:
+        batch = get_time_batch(float(item["lastVisitTime"]))
+        category = item["category"].split(":")[1].strip()  # To get the actual category name
+        category_group = item["category_group"]
+        
+        extracted = tldextract.extract(item["url"])
+        domain = "{}.{}".format(extracted.domain, extracted.suffix)
+        item["icon"] = "https://www.google.com/s2/favicons?domain={}&sz=48".format(domain)
+        
+        
+        transformed_data = {
+            "domain": domain,
+            "name": item["title"],
+            "visit": item["visitCount"],
+            "icon": item["icon"],
+            "subcategory": category,
+            "Category Description": item["category_description"]
+        }
+
+        # Check if category exists in the batch
+        category_exists = False
+        for cat in result[batch]:
+            if category_group in cat:
+                cat[category_group].append(transformed_data)
+                category_exists = True
+                break
+
+        # If category doesn't exist, create a new one
+        if not category_exists:
+            result[batch].append({category_group: [transformed_data]})
+
+    return result
+
+
+# def get_grpah_query(user_id, fromEpoch, toEpoch, divisions):
+#     table = dynamodb.Table('history')
+#     fromEpoch = Decimal(fromEpoch)
+#     toEpoch = Decimal(toEpoch)
+#     divisions = Decimal(divisions)
+#     response = table.scan(
+#     FilterExpression="user_id = :user_id AND #visitTime BETWEEN :fromEpoch AND :toEpoch",
+#     ExpressionAttributeNames={
+#         "#visitTime": "visitTime",
+#     },
+#     ExpressionAttributeValues={
+#         ":user_id": user_id,
+#         ":fromEpoch": fromEpoch,
+#         ":toEpoch": toEpoch
+#     }
+# )
     
-    items = response['Items']
-    division_length = (toEpoch - fromEpoch) / divisions
-    results = defaultdict(dict)
-    for item in items:
-        division_index = int((int(item["visitTime"]) - fromEpoch) / division_length)
-        print(division_index)
-        category = item['category']
-        if category not in results[division_index]:
-            results[division_index][category] = []
-        results[division_index][category].append(item)
-    return results
+#     items = response['Items']
+#     division_length = (toEpoch - fromEpoch) / divisions
+#     results = defaultdict(dict)
+#     for item in items:
+#         division_index = int((int(item["visitTime"]) - fromEpoch) / division_length)
+#         print(division_index)
+#         category = item['category']
+#         if category not in results[division_index]:
+#             results[division_index][category] = []
+#         results[division_index][category].append(item)
+    
+    
+#     # hourly -> 24/4 => 00-04, 04-08,08-12,12-16,16-20,20-24  
+#     # weekly -> monday, tuesday, wednesday, thursday, friday
+#     # monthly -> week 1, week 2, week 3, week 4, week 5
+#     # Last 6 months -> May, June, July, August, September, October 
+#     {
+#         "00-04": [
+#             {"category_name 1":[
+#                 {
+#                 "domain": "domain url",
+#                 "name": "title",
+#                 "visit": "number of visits in this day/hour/week/month",
+#                 "icon": "icon url",
+#                 "subcategory": "Sub category",
+#                 "Category Description": "Category Description"
+#                 },
+#                 {
+#                 "domain": "domain url",
+#                 "name": "title",
+#                 "visit": "number of visits in this day/hour/week/month",
+#                 "icon": "icon url",
+#                 "subcategory": "Sub category",
+#                 },
+#                 "..."
+#                 ]},
+#             {"category_name 2":[
+#                 {
+#                 "domain": "domain url",
+#                 "name": "title",
+#                 "visit": "number of visits in this day/hour/week/month",
+#                 "icon": "icon url",
+#                 "subcategory": "Sub category",
+#                 },"..."]
+#                 },
+#             ],
+#         "04-08": same as 00-04
+#         }    
+    
+#     return results
 
 def get_entire_profile(user_id):
     dynamodb = boto3.resource('dynamodb')
@@ -182,14 +304,17 @@ def get_pinned_website(user_id):
     # Return the items from the response
     return response['Items']
 
-def add_to_pinned_websites(user_id, domain_name, order):
+def add_to_pinned_websites(user_id, domain_name, order, title):
     table = dynamodb.Table('pinned_websites')
     
     item = {
         'user_id': user_id,
-        'domain_name': domain_name,
+        'domain_name': domain_name, # www.kleo.network
         'order': order,
-        'id':  str(uuid.uuid4())
+        'name': title,
+        'id':  str(uuid.uuid4()),
+        'icon': "https://www.google.com/s2/favicons?domain={}&sz=48".format(domain_name)
+
     }
     
     response = table.put_item(Item=item)
