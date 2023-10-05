4cabb0047334369d3f5dee3ae6a1101542f6ef3d
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2023-10-05 23:01:59 +0530

    add for year

--- app/core/models/graph.py
+++ app/core/models/graph.py
@@ -1,9 +1,6 @@
 import json
 from datetime import datetime
 import boto3
-# Sample history data
-from decimal import Decimal
-
 import math
 
 aws_access_key = "AKIA3RWDXTFSIADMEAPE"
@@ -20,9 +17,8 @@ dynamodb = session.resource('dynamodb')
 def get_domain(url):
     return url.split("//")[-1].split("/")[0].split("?")[0]
 
-# Grouping by hours of the day
 def group_by_hour(timestamp):
-    dt_object = datetime.utcfromtimestamp(timestamp/1000)  # Convert to seconds
+    dt_object = datetime.utcfromtimestamp(timestamp/1000)
     hour = dt_object.hour
     if 0 <= hour < 4:
         return "00-04"
@@ -37,55 +33,103 @@ def group_by_hour(timestamp):
     elif 20 <= hour < 24:
         return "20-24"
 
-# Grouping by days of the week
 def group_by_day(timestamp):
     dt_object = datetime.utcfromtimestamp(timestamp/1000)
     return dt_object.strftime('%A')
 
-# Grouping by weeks of the month
 def group_by_week(timestamp):
     dt_object = datetime.utcfromtimestamp(timestamp/1000)
     day_of_month = dt_object.day
     week_of_month = math.ceil(day_of_month / 7.0)
     return f"Week {week_of_month}"
 
+def group_by_month(timestamp):
+    dt_object = datetime.utcfromtimestamp(timestamp/1000)
+    return dt_object.strftime('%B')
+
 grouping_methods = {
     'hour': group_by_hour,
     'day': group_by_day,
-    'week': group_by_week
+    'week': group_by_week,
+    'month': group_by_month
+}
+
+def initialize_output_for_hours():
+    return {
+        "00-04": {},
+        "04-08": {},
+        "08-12": {},
+        "12-16": {},
+        "16-20": {},
+        "20-24": {}
+    }
+
+def initialize_output_for_days():
+    return {
+        "Monday": {},
+        "Tuesday": {},
+        "Wednesday": {},
+        "Thursday": {},
+        "Friday": {},
+        "Saturday": {},
+        "Sunday": {}
+    }
+
+def initialize_output_for_weeks():
+    return {
+        "Week 1": {},
+        "Week 2": {},
+        "Week 3": {},
+        "Week 4": {},
+        "Week 5": {}
+    }
+
+def initialize_output_for_year():
+    return {
+        "January": {},
+        "February": {},
+        "March": {},
+        "April": {},
+        "May": {},
+        "June": {},
+        "July": {},
+        "August": {},
+        "September": {},
+        "October": {},
+        "November": {},
+        "December": {}
+    }
+
+initialization_methods = {
+    'hour': initialize_output_for_hours,
+    'day': initialize_output_for_days,
+    'week': initialize_output_for_weeks,
+    'month': initialize_output_for_year
 }
 
 def process_data(group_by, history_data):
-    # Choose the grouping method based on the parameter
     group_function = grouping_methods[group_by]
-
-    # Empty output data
-    output_data = {}
+    output_data = initialization_methods[group_by]()
 
     for entry in history_data:
         group_value = group_function(int(entry["lastVisitTime"]))
         category = entry["category"]
         domain = get_domain(entry["url"])
-        icon = entry.get("icon", "https://www.google.com/s2/favicons?domain={}&sz=48".format(domain))
+        icon = entry.get("icon", f"https://www.google.com/s2/favicons?domain={domain}&sz=48")
         name = domain
-
-        if group_value not in output_data:
-            output_data[group_value] = {}
 
         if category not in output_data[group_value]:
             output_data[group_value][category] = {
                 "domains": {},
                 "totalCategoryVisits": 0
             }
-        
+
         if domain not in output_data[group_value][category]["domains"]:
             output_data[group_value][category]["domains"][domain] = {"visitCounterTimeRange": 0, "icon": icon, "name": name}
 
-        
         output_data[group_value][category]["domains"][domain]["visitCounterTimeRange"] += 1
         output_data[group_value][category]["totalCategoryVisits"] += 1
 
-    # Convert domain data to desired output format
     for group_value, categories in output_data.items():
         for category, data in categories.items():
             domains_list = [{"domain": k, "visitCounterTimeRange": v["visitCounterTimeRange"], "icon": v["icon"],  "name": v["name"]} for k, v in data["domains"].items()]
@@ -93,7 +137,6 @@ def process_data(group_by, history_data):
 
     return output_data
 
-# To use:
 def graph_query(group_by_parameter):
     table = dynamodb.Table('history')
     response = table.scan()
