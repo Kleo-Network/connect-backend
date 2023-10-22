775b5c8b254549550372aef216124b0def567e5f
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2023-10-23 04:12:38 +0530

    authentication is working

--- app/core/controllers/user.py
+++ app/core/controllers/user.py
@@ -5,7 +5,28 @@ import json
 
 from ..models.aws_session import dynamodb
 
+def update_user_nonce(id, nonce):
+    table = dynamodb.Table('users')
+    response = table.update_item(
+            Key={
+                'id': id  # your primary key column name and value
+            },
+            UpdateExpression="SET nonce = :val",  # Update the 'nonce' attribute
+            ExpressionAttributeValues={
+                ':val': nonce  # value that 'nonce' will be set to
+            },
+            ReturnValues="UPDATED_NEW"  # returns the item attributes as they appear after the update
+        )
+    return response
 
+def check_user_and_return(address):
+    table = dynamodb.Table('users')
+    response = table.get_item(Key={'address': address})
+    if 'Item' in response:
+        return response['Item']
+    else:
+        return None
+    
 def create_user(user):
     table = dynamodb.Table('users')
     response = table.put_item(Item=user)
