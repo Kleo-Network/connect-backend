cddfdf0a04b8a088134cf91bcdf3fcb31f94b433
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2023-11-09 15:55:57 +0000

    commit run.py for deployment

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
+
+
 from app import create_app
 
 app, celery = create_app()
 app.app_context().push()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=5001)
+    app.run()
+    #app.run(host='0.0.0.0', debug=True, port=5001)
