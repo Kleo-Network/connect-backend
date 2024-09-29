fa59abd28dec6eeec4d12690afc8d88287bb9b1f
princedalsaniya <prince.d@ahduni.edu.in>
2024-09-29 23:21:16 +0530

    restored API for getting activity from content

--- app/core/views/user_v2_views.py
+++ app/core/views/user_v2_views.py
@@ -88,3 +88,13 @@ def allocate_vana_tokens(wallet_address):
             f"Failed to allocate Vana token to address: {wallet_address}. Error: {str(e)}"
         )
         return 500  # Internal Server Error
+
+
+@core.route("/activity_classification", methods=["POST"])
+def classify_content():
+    data = request.json
+
+    activity = get_most_relevant_activity(data["content"])
+    return jsonify(
+        {"response": f"Most relevant activity for the content given is {activity}"}
+    )
