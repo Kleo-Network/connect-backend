623e2dd2cf22bf6bce65e23e20562eba6414c0f4
princedalsaniya <prince.d@ahduni.edu.in>
2024-11-02 23:41:21 +0530

    refactor: updated runLocal script to use python3

--- runLocal.sh
+++ runLocal.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Step 1: Format the entire project with Black
-echo "Formatting the project with Black..."
-black .
+# echo "Formatting the project with Black..."
+# black .
 
 # Step 2: Set the environment variable for Flask to run in debug mode
 export FLASK_ENV=development
 
 # Step 3: Run the Flask app (from run.py) in debug mode
 echo "Starting the Flask application..."
-python run.py
+python3 run.py
