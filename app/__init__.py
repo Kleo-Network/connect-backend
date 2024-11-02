110b48459ba618a443afda4b571c29bffce53abf
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2024-11-03 04:25:22 +0530

    the night is young

--- app/__init__.py
+++ app/__init__.py
@@ -26,21 +26,9 @@ def register_blueprints(app):
     """
 
     # Import blueprints
-    from .core.views.user_views import core as core_user
     from .core.views.user_v2_views import core as core_user_v2
-    from .core.views.auth_views import core as core_auth
-    from .core.views.history_views import core as core_history
-    from .core.views.admin_views import core as admin_views
 
     # Register blueprints with proper versioned URL prefixes
-    app.register_blueprint(core_user, name="user_api", url_prefix="/api/v1/core/user")
     app.register_blueprint(
         core_user_v2, name="user_api_v2", url_prefix="/api/v2/core/user"
-    )
-    app.register_blueprint(core_auth, name="auth_api", url_prefix="/api/v1/core/auth")
-    app.register_blueprint(
-        core_history, name="history_api", url_prefix="/api/v1/core/history"
-    )
-    app.register_blueprint(
-        admin_views, name="admin_api", url_prefix="/api/v1/core/admin"
     )
